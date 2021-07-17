4559be0d9343ed7e48168c84a26faf5e233f99ae
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2021-07-18 02:25:37 +0700

    Update loading_screen.py

--- loading_screen.py
+++ loading_screen.py
@@ -14,16 +14,11 @@ class LoadingScreen:
         # Prevents closing
         self.root.protocol('WM_DELETE_WINDOW', lambda: None)
         # self.root.attributes('-fullscreen', True)
-        label = ttk.Label(self.root, text='Pika Center Invoicing Program', font=("Calibri", "36", "bold  "))
+        label = ttk.Label(self.root, text='Pika Center Invoicing Program', font=("Calibri", "36", "bold"))
         label.grid(column=0, row=0)
-    
-        self.progress_label = StringVar()
-        progress_label = ttk.Label(self.root, textvariable=self.progress_label)
-        progress_label.grid(column=0, row=1, sticky=(W))
 
         self.root.columnconfigure(0, weight=1)
-        self.root.rowconfigure(0, weight=5)
-        self.root.rowconfigure(1, weight=1)
+        self.root.rowconfigure(0, weight=1)
         self.root.after(2000, lambda: self.root.destroy())
 
         self.root.mainloop()
