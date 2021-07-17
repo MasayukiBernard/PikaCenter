935f0bed0c0563ec90a6f51b1adfd23fe972e689
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2021-07-18 02:23:21 +0700

    Update add_product.py
    - Added window hierarchy mechanism
    - Add detail new row
    - Delete detail row
    - Autocorrect numerical entry
    - Validate form
    - Save form

--- add_product.py
+++ add_product.py
@@ -1,11 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 
+from functools import partial
 from pg8000.native import Connection
 import tools
 
+from alert import Alert
+from confirmation import Confirmation
+
 class AddProduct:
-    def __init__(self, window_status, db_password):
+    def __init__(self, window_status, db_password, parent_obj):
         self.db_password = db_password
         window_size = {'width': '800', 'height': '450'}
         self.monitor_actual_area = tools.get_monitor_actual_area()
@@ -14,10 +18,14 @@ class AddProduct:
         self.frame_height = int(window_size['height']) - (2 * self.pad_val)
         self.window_status = window_status
 
+        window_status['manage_product'] = True
+        self.window_status = window_status
+        self.parent_obj = parent_obj
+
         self.root = Tk()
         self.root.title("Pika Center Invoicing Program - Add Product")
         self.root.geometry(tools.generate_tk_geometry(window_size))
-        self.root.protocol('WM_DELETE_WINDOW', tools.change_window_status(self.window_status, 'is_closed', True))
+        self.root.protocol('WM_DELETE_WINDOW', self.close_window)
         self.root.resizable(False, False)
 
         self.canvas_frame = ttk.Frame(self.root)
@@ -29,7 +37,7 @@ class AddProduct:
         self.main_frame = ttk.Frame(self.canvas, borderwidth=2, relief='groove')
         self.title_label = ttk.Label(self.main_frame, text="Formulir Penambahan Produk", font=("Calibri", "16"), borderwidth=2, relief='groove')
         
-        self.window_height = self.frame_height + 2000
+        self.window_height = self.frame_height + 4000
         self.window_id = self.canvas.create_window((0,0), window=self.main_frame, anchor="nw", width=self.frame_width, height=self.window_height)
 
         self.form_vars = {
@@ -67,19 +75,19 @@ class AddProduct:
                 }
             },
         }
-        
+
         self.form_detail_vars = {
-            'sku': [StringVar()],
-            'temp_invoice_id': [StringVar()],
-            'buyprice': [StringVar()],
-            'sellprice': [StringVar()],
+            'sku': [],
+            'temp_invoice_id': [],
+            'buyprice': [],
+            'sellprice': [],
         }
         self.form_detail_widgets = {}
         self.form_detail_widgets['frame'] = {
             'title': ttk.Frame(self.form_frame, borderwidth=2, relief='groove', width=self.frame_width),
             'heading': {'base': ttk.Frame(self.form_frame, borderwidth=2, relief='groove')},
-            'entry': [ttk.Frame(self.form_frame, borderwidth=2, relief='groove')],
-            'add_row': ttk.Frame(self.form_frame, borderwidth=2, relief='groove')
+            'entry': [],
+            'btns_row': ttk.Frame(self.form_frame, borderwidth=2, relief='groove')
         }
         self.form_detail_widgets['frame']['heading']['column'] ={
             'sku': ttk.Frame(self.form_detail_widgets['frame']['heading']['base'], borderwidth=2, relief='groove'),
@@ -99,18 +107,20 @@ class AddProduct:
             }
         }
         self.form_detail_widgets['entry'] = {
-            'sku': [ttk.Entry(self.form_detail_widgets['frame']['entry'][0], justify='center', textvariable=self.form_detail_vars['sku'][0])],
-            'temp_invoice_id': [ttk.Entry(self.form_detail_widgets['frame']['entry'][0], justify='center', textvariable=self.form_detail_vars['temp_invoice_id'][0])],
-            'buyprice': [ttk.Entry(self.form_detail_widgets['frame']['entry'][0], justify='center', textvariable=self.form_detail_vars['buyprice'][0])],
-            'sellprice': [ttk.Entry(self.form_detail_widgets['frame']['entry'][0], justify='center', textvariable=self.form_detail_vars['sellprice'][0])],
+            'sku': [],
+            'temp_invoice_id': [],
+            'buyprice': [],
+            'sellprice': [],
         }
-        self.form_detail_widgets['del_btn'] = [ttk.Button(self.form_detail_widgets['frame']['entry'][0], text='X',width=3)]
-        self.form_detail_widgets['add_row_btn'] = ttk.Button(self.form_detail_widgets['frame']['add_row'], text='Tambah', command=self.add_new_row, width=10)
+        self.form_detail_widgets['del_btn'] = []
+        self.form_detail_widgets['add_row_btn'] = ttk.Button(self.form_detail_widgets['frame']['btns_row'], text='Tambah', command=self.add_new_row, width=10)
+        self.form_detail_widgets['save_btn'] = ttk.Button(self.form_detail_widgets['frame']['btns_row'], text='SIMPAN', command=self.save_product, width=10)
+        
 
         self.canvas_frame.grid(column=0, row=0, sticky=(N, E, S, W))
         self.canvas.grid(column=0, row=0, sticky=(N, E, S, W))
         self.canvas_sb.grid(column=1, row=0, sticky=(N,S,E))
-        # self.main_frame.grid(column=0, row=0, sticky=(N, E, S, W))
+        
         self.title_label.grid(column=0, row=0, pady=self.pad_val*2)
 
         self.form_frame.grid(column=0, row=1, sticky=(N, W, E, S), pady=(self.pad_val*2, 0))
@@ -134,9 +144,7 @@ class AddProduct:
         self.form_detail_widgets['frame']['heading']['column']['buyprice'].grid(column=2, row=0, sticky=(W, E))
         self.form_detail_widgets['frame']['heading']['column']['sellprice'].grid(column=3, row=0, sticky=(W, E))
         self.form_detail_widgets['frame']['heading']['column']['del_btn'].grid(column=4, row=0, sticky=(W, E))
-        self.form_detail_widgets['frame']['entry'][0].grid(column=0, row=4, sticky=(W, E))
-        self.form_detail_widgets['frame']['add_row'].grid(column=0, row=5, sticky=(W, E))
-        self.current_row = 5
+        self.current_row = 4
 
         self.form_detail_widgets['label']['title'].grid(column=0, row=0)
         self.form_detail_widgets['label']['heading']['sku'].grid(column=0, row=0)
@@ -144,14 +152,6 @@ class AddProduct:
         self.form_detail_widgets['label']['heading']['buyprice'].grid(column=0, row=0)
         self.form_detail_widgets['label']['heading']['sellprice'].grid(column=0, row=0)
         self.form_detail_widgets['label']['heading']['del_btn'].grid(column=0, row=0)
-
-        self.form_detail_widgets['entry']['sku'][0].grid(column=0, row=0, sticky=(W), padx=(self.pad_val+3,0))
-        self.form_detail_widgets['entry']['temp_invoice_id'][0].grid(column=1, row=0, sticky=(W), padx=(self.pad_val*3, 0), ipadx=self.pad_val*2)
-        self.form_detail_widgets['entry']['buyprice'][0].grid(column=2, row=0, sticky=(W), padx=(self.pad_val,0), ipadx=self.pad_val*2)
-        self.form_detail_widgets['entry']['sellprice'][0].grid(column=3, row=0, sticky=(W), ipadx=self.pad_val*2)
-        self.form_detail_widgets['del_btn'][0].grid(column=4, row=0, sticky=(W), padx=(0, self.pad_val*2))
-
-        self.form_detail_widgets['add_row_btn'].grid(column=0, row=0, sticky=(W), padx=(self.pad_val+2, 0))
 
         for child in self.form_frame.winfo_children():
             child.grid_configure(pady=(self.pad_val*2, 0))
@@ -183,43 +183,170 @@ class AddProduct:
         self.form_detail_widgets['frame']['heading']['column']['buyprice'].grid_columnconfigure(0, weight=1)
         self.form_detail_widgets['frame']['heading']['column']['sellprice'].grid_columnconfigure(0, weight=1)
         self.form_detail_widgets['frame']['heading']['column']['del_btn'].grid_columnconfigure(0, weight=1)
-        self.form_detail_widgets['frame']['entry'][0].grid_columnconfigure(0, weight=6)
-        self.form_detail_widgets['frame']['entry'][0].grid_columnconfigure(1, weight=10)
-        self.form_detail_widgets['frame']['entry'][0].grid_columnconfigure(2, weight=9)
-        self.form_detail_widgets['frame']['entry'][0].grid_columnconfigure(3, weight=8)
-        self.form_detail_widgets['frame']['entry'][0].grid_columnconfigure(4, weight=1)
-        self.form_detail_widgets['frame']['add_row'].grid_columnconfigure(0, weight=1)
         
+        self.add_new_row()
+
         self.root.mainloop()
     
+    def close_window(self, *args):
+        tools.change_window_status(self.window_status, 'is_closed', True)
+        self.root.destroy()
+        self.parent_obj.__init__(self.parent_obj.window_status, self.db_password)
+
+    def correct_numeric_entry(self, *args):
+        key = args[0]
+        selected_entry_idx = args[1]
+        entered_str = self.form_detail_vars[key][selected_entry_idx].get()
+        print(key, selected_entry_idx, entered_str)
+        if len(entered_str) > 0:
+            res_str = ''
+            res_str = tools.remove_non_integer(entered_str)
+            if len(res_str) > 0:
+                res_str = tools.create_pretty_numerical(int(res_str))
+                self.form_detail_vars[key][selected_entry_idx].set(res_str)
+            else:
+                self.form_detail_vars[key][selected_entry_idx].set('')
+            
+
     def add_new_row(self, *args):
-        current_len = self.current_row - 4
+        current_len = len(self.form_detail_widgets['del_btn'])
+
         self.form_detail_widgets['frame']['entry'].append(ttk.Frame(self.form_frame, borderwidth=2, relief='groove'))
         self.form_detail_vars['sku'].append(StringVar())
         self.form_detail_widgets['entry']['sku'].append(ttk.Entry(self.form_detail_widgets['frame']['entry'][current_len], justify='center', textvariable=self.form_detail_vars['sku'][current_len]))
         self.form_detail_vars['temp_invoice_id'].append(StringVar())
         self.form_detail_widgets['entry']['temp_invoice_id'].append(ttk.Entry(self.form_detail_widgets['frame']['entry'][current_len], justify='center', textvariable=self.form_detail_vars['temp_invoice_id'][current_len]))
+
         self.form_detail_vars['buyprice'].append(StringVar())
-        self.form_detail_widgets['entry']['buyprice'].append(ttk.Entry(self.form_detail_widgets['frame']['entry'][current_len], justify='center', textvariable=self.form_detail_vars['buyprice'][current_len]))
+        temp_bp = ttk.Entry(self.form_detail_widgets['frame']['entry'][current_len], justify='center', textvariable=self.form_detail_vars['buyprice'][current_len])
+        temp_bp.bind("<KeyRelease>", partial(self.correct_numeric_entry, 'buyprice', current_len))
+        self.form_detail_widgets['entry']['buyprice'].append(temp_bp)
+
         self.form_detail_vars['sellprice'].append(StringVar())
-        self.form_detail_widgets['entry']['sellprice'].append(ttk.Entry(self.form_detail_widgets['frame']['entry'][current_len], justify='center'   , textvariable=self.form_detail_vars['sellprice'][current_len]))
-        self.form_detail_widgets['del_btn'].append(ttk.Button(self.form_detail_widgets['frame']['entry'][current_len], text='X',width=3))
+        temp_sp = ttk.Entry(self.form_detail_widgets['frame']['entry'][current_len], justify='center', textvariable=self.form_detail_vars['sellprice'][current_len])
+        temp_sp.bind("<KeyRelease>", partial(self.correct_numeric_entry, 'sellprice', current_len))
+        self.form_detail_widgets['entry']['sellprice'].append(temp_sp)
+
+        self.form_detail_widgets['del_btn'].append(ttk.Button(self.form_detail_widgets['frame']['entry'][current_len], text='X',width=3, command=lambda: self.delete_row(current_len)))
 
         self.form_detail_widgets['frame']['entry'][current_len].grid(column=0, row=self.current_row, sticky=(W, E))
         self.form_detail_widgets['entry']['sku'][current_len].grid(column=0, row=0, sticky=(W), padx=(self.pad_val+3,0))
-        self.form_detail_widgets['entry']['temp_invoice_id'][current_len].grid(column=1, row=0, sticky=(W), padx=(self.pad_val*3, 0), ipadx=self.pad_val*2)
+        self.form_detail_widgets['entry']['temp_invoice_id'][current_len].grid(column=1, row=0, sticky=(W), padx=(self.pad_val*2, 0), ipadx=self.pad_val*2)
         self.form_detail_widgets['entry']['buyprice'][current_len].grid(column=2, row=0, sticky=(W), padx=(self.pad_val,0), ipadx=self.pad_val*2)
         self.form_detail_widgets['entry']['sellprice'][current_len].grid(column=3, row=0, sticky=(W), ipadx=self.pad_val*2)
         self.form_detail_widgets['del_btn'][current_len].grid(column=4, row=0, sticky=(W), padx=(0, self.pad_val*2))
 
         self.current_row += 1
 
-        self.form_detail_widgets['frame']['add_row'].grid(column=0, row=self.current_row, sticky=(W, E))
+        self.form_detail_widgets['frame']['btns_row'].grid(column=0, row=self.current_row, sticky=(W))
         self.form_detail_widgets['add_row_btn'].grid(column=0, row=0, sticky=(W), padx=(self.pad_val+2, 0))
+        self.form_detail_widgets['save_btn'].grid(column=1, row=0, sticky=(W), padx=(self.pad_val, 0))
 
         self.form_detail_widgets['frame']['entry'][current_len].grid_columnconfigure(0, weight=6)
         self.form_detail_widgets['frame']['entry'][current_len].grid_columnconfigure(1, weight=10)
         self.form_detail_widgets['frame']['entry'][current_len].grid_columnconfigure(2, weight=9)
         self.form_detail_widgets['frame']['entry'][current_len].grid_columnconfigure(3, weight=8)
         self.form_detail_widgets['frame']['entry'][current_len].grid_columnconfigure(4, weight=1)
-        self.form_detail_widgets['frame']['add_row'].grid_columnconfigure(0, weight=1)
+        self.form_detail_widgets['frame']['btns_row'].grid_columnconfigure(0, weight=1)
+        self.form_detail_widgets['frame']['btns_row'].grid_columnconfigure(1, weight=1)
+    
+    def delete_row(self, *args):
+        current_len = len(self.form_detail_widgets['del_btn'])
+        if current_len > 1:
+            row_idx = args[0]
+
+            for key, val in self.form_detail_widgets['entry'].items():
+                val[row_idx].destroy()
+                del val[row_idx]
+
+            for key, val in self.form_detail_vars.items():
+                del val[row_idx]
+            
+            self.form_detail_widgets['frame']['entry'][row_idx].destroy()
+            del self.form_detail_widgets['frame']['entry'][row_idx]
+
+            self.form_detail_widgets['del_btn'][row_idx].destroy()
+            del self.form_detail_widgets['del_btn'][row_idx]
+
+            new_current_len = len(self.form_detail_widgets['del_btn'])
+
+            for i in range(new_current_len):
+                self.form_detail_widgets['del_btn'][i].configure(command=partial(self.delete_row, i))
+        elif current_len == 1:
+            for key, val in self.form_detail_vars.items():
+                val[0].set('')
+        
+    def validate_inputs(self):
+        is_passed = True
+        keys = ['sku', 'temp_invoice_id', 'buyprice', 'sellprice']
+
+        if len(self.form_vars['product']['name'].get()) == 0:
+            is_passed = False
+        
+        res_len = len(self.form_detail_vars['sku'])
+        row_idx_to_del = []
+        for i in range(res_len):
+            is_not_empty = False
+            for key in keys:
+                if len(self.form_detail_vars[key][i].get()) > 0:
+                    is_not_empty = True
+            
+            if not is_not_empty:
+                row_idx_to_del.append(i)
+
+        num_of_del = 0
+        for idx in row_idx_to_del:
+            self.delete_row((idx-num_of_del))
+            num_of_del+=1
+        
+        return is_passed
+
+    def save_product(self, *args):
+        input_valid = self.validate_inputs()
+        if not input_valid:
+            Alert('Nama harus diisi!')
+            return
+
+        res_list = [None]
+        Confirmation("Product", "Konfirmasi Penambahan Produk", res_list=res_list)
+        confirmed = res_list[0]
+
+        if confirmed:
+            for key, val in self.form_detail_vars.items():
+                print(key)
+                print('len:', len(val))
+                for item in val:
+                    print(item.get())
+            print("\n")
+            
+            
+            insert_dict = {}
+            insert_sql = "INSERT INTO public.products (pkey, name, description) VALUES (:generated_uuid, :name, :description);"
+            insert_dict['name'] = self.form_vars['product']['name'].get().replace("\n", "")
+            insert_dict['description'] = self.form_vars['product']['description'].get().replace("\n", "")
+            conn = Connection(user="postgres", password=self.db_password, database="pikacenter")
+            generated_uuid = conn.run("SELECT uuid_generate_v1();")[0][0]
+            insert_dict['generated_uuid'] = generated_uuid
+            conn.run(insert_sql, **insert_dict)
+
+            res_len = len(self.form_detail_vars['sku'])
+            
+            for i in range(res_len):
+                insert_dict = {}
+                insert_sql = ""
+                insert_sql += "INSERT INTO public.products_details(pkey, refproductkey, sku, temp_invoice_id, buyprice, sellprice) VALUES(uuid_generate_v1(), :generated_uuid, :sku_"+str(i)+", :temp_invoice_id_"+str(i)+", :buyprice_"+str(i)+", :sellprice_"+str(i)+");"
+                
+                insert_dict['generated_uuid'] = generated_uuid
+                insert_dict['sku_'+str(i)] = self.form_detail_vars['sku'][i].get()
+                insert_dict['temp_invoice_id_'+str(i)] = self.form_detail_vars['temp_invoice_id'][i].get()
+                bp_str = str(self.form_detail_vars['buyprice'][i].get())
+                bp_str = bp_str.replace(',', '')
+                insert_dict['buyprice_'+str(i)] = bp_str
+                sp_str = str(self.form_detail_vars['sellprice'][i].get())
+                sp_str = sp_str.replace(',', '')
+                insert_dict['sellprice_'+str(i)] = sp_str
+                
+                conn = Connection(user="postgres", password=self.db_password, database="pikacenter")
+                conn.run(insert_sql, **insert_dict)
+
+        self.close_window()
