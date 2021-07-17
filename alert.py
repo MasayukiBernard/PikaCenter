a9f0f104c7c354e104266870994a5d8be6560db2
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2021-07-18 02:25:42 +0700

    Update alert.py

--- alert.py
+++ alert.py
@@ -6,12 +6,12 @@ class Alert:
     def __init__(self, message):
         window_size = {'width': "450", 'height': "50"}
 
-        root = Tk()
-        root.title("Pika Center Invoicing Program - Alert")
-        root.geometry(tools.generate_tk_geometry(window_size))
-        root.resizable(False, False)
+        self.root = Tk()
+        self.root.title("Pika Center Invoicing Program - Alert")
+        self.root.geometry(tools.generate_tk_geometry(window_size))
+        self.root.resizable(False, False)
 
-        main_frame = ttk.Frame(root)
+        main_frame = ttk.Frame(self.root)
 
         label = ttk.Label(main_frame, text=message, foreground='red')
         label.grid(column=0, row=0)
@@ -19,9 +19,9 @@ class Alert:
         main_frame.grid(column=0, row=0, rowspan=2, sticky=(N, W, E, S))
         label.grid(column=0, row=0)
 
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
         main_frame.columnconfigure(0, weight=1)
         main_frame.rowconfigure(0, weight=1)
 
-        root.mainloop()
+        self.root.mainloop()
