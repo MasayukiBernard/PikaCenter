3a44109130375134279f7953240d76d96f91310f
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2021-07-18 02:25:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import windows
 from alert import Alert
+from inventory import Inventory
 from loading_screen import LoadingScreen
 from prompt_db_pass import PromptDatabasePassword
 
@@ -10,19 +11,25 @@ import tools
 
 if __name__ == '__main__':
     # Fire up postgreSql service, if not yet on
-    load_screen = LoadingScreen()
+    # load_screen = LoadingScreen()
 
-    password = {'inputted': ""}
-    is_passed = False
-    conn = None
+    password = {'inputted': "password"}
+    windows_status = {
+        'promptdbpass': {'is_closed': True},
+        'inventory': {'is_closed': False},
+    }
     
-    while not is_passed:
-        PromptDatabasePassword(password)
+    while not windows_status['promptdbpass']['is_closed']:
+        # PromptDatabasePassword(window_status['promptdbpass'], password)
         try:
-            conn = Connection("postgres", password=password['inputted'])
+            conn = Connection(user="postgres", password=password['inputted'], database="pikacenter")
+            conn.run("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
         except Exception as e:
-            Alert('Kata sandi pengguna database salah!')
+            if not windows_status['promptdbpass']['is_closed']:
+                Alert('Database belum nyala / Kata sandi pengguna database salah!')
             continue
-
-        is_passed = True
+    
+    
+    while not windows_status['inventory']['is_closed']:
+        Inventory(windows_status['inventory'], db_password=password['inputted'])
     # Stop postgresql service
