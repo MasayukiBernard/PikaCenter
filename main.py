6478dd42832f886ed2d787384ab4621ec4c7bed2
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2021-07-18 20:30:48 +0700

    Fixed windows_statuses

--- main.py
+++ main.py
@@ -6,30 +6,46 @@ from prompt_db_pass import PromptDatabasePassword
 
 # Import dependencies
 from pg8000.native import Connection
-import tools
+import ctypes
+import os
 
+def try_connecting_to_db(window_status):
+    is_success = False
+    PromptDatabasePassword(window_status, password)
+    try:
+        conn = Connection(user="postgres", password=password['inputted'], database="pikacenter")
+        conn.run("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
+        is_success = True
+    except Exception as e:
+        if not window_status['is_closed']:
+            Alert('Database belum nyala / Kata sandi pengguna database salah!')
+    
+    if windows_status['promptdbpass']['is_closed']:
+        exit(1)
+    
+    return is_success
 
 if __name__ == '__main__':
-    # Fire up postgreSql service, if not yet on
-    # load_screen = LoadingScreen()
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        # Fire up postgreSql service, if not yet on
+        command_res = os.system("sc start postgresql-x64-13")
 
-    password = {'inputted': "password"}
-    windows_status = {
-        'promptdbpass': {'is_closed': True},
-        'inventory': {'is_closed': False},
-    }
-    
-    while not windows_status['promptdbpass']['is_closed']:
-        # PromptDatabasePassword(window_status['promptdbpass'], password)
-        try:
-            conn = Connection(user="postgres", password=password['inputted'], database="pikacenter")
-            conn.run("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
-        except Exception as e:
-            if not windows_status['promptdbpass']['is_closed']:
-                Alert('Database belum nyala / Kata sandi pengguna database salah!')
-            continue
-    
-    
-    while not windows_status['inventory']['is_closed']:
+        load_screen = LoadingScreen()
+
+        password = {'inputted': ""}
+        windows_status = {
+            'promptdbpass': {'is_closed': True},
+            'inventory': {'is_closed': True},
+        }
+        
+        while not try_connecting_to_db(windows_status['promptdbpass']): pass
+        
         Inventory(windows_status['inventory'], db_password=password['inputted'])
-    # Stop postgresql service
+        if windows_status['inventory']['is_closed']:
+            # Stop postgresql service
+            command_res = os.system("sc stop postgresql-x64-13")
+        else:
+            pass
+
+    else:
+        pass
