9797ed5cea4650b28c2cc983d6d2f1e4cf8f4eba
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2021-07-24 01:36:19 +0700

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -15,7 +15,7 @@ from manage_product import ManageProduct
 class Inventory:
     def __init__(self, window_status, parent_child_roots_list, db_password):
         self.db_password = db_password
-        window_size = {'width': '1280', 'height': '720'}
+        window_size = {'width': '1280', 'height': '700'}
         self.monitor_actual_area = tools.get_monitor_actual_area()
         self.pad_val = 7
         self.frame_width = int(window_size['width']) - (2 * self.pad_val)
@@ -38,13 +38,13 @@ class Inventory:
         self.main_frame = ttk.Frame(self.root, width=self.frame_width)
         self.window_title = ttk.Label(self.main_frame, text="INVENTORY", anchor='center', font=("Calibri", "28", "bold"))
         self.product_search_var = StringVar()
-        placeholder_val = ". . . Search by Name or Description"
-        self.product_search_var.set(placeholder_val)
+        self.placeholder_val = ". . . Search by Name or Description"
+        self.product_search_var.set(self.placeholder_val)
         search_bar_style = ttk.Style()
         search_bar_style.configure('ipadded.TEntry', padding=[self.pad_val, 0, self.pad_val, 0])
         self.product_search_bar = ttk.Entry(self.main_frame, textvariable=self.product_search_var, width=48, foreground='#A9A9A9', justify='right', style='ipadded.TEntry')
-        self.product_search_bar.bind('<FocusIn>', partial(self.manage_placeholder, 'in', self.product_search_var, placeholder_val))
-        self.product_search_bar.bind('<FocusOut>', partial(self.manage_placeholder, 'out', self.product_search_var, placeholder_val))
+        self.product_search_bar.bind('<FocusIn>', partial(self.manage_placeholder, 'in', self.product_search_var, self.placeholder_val))
+        self.product_search_bar.bind('<FocusOut>', partial(self.manage_placeholder, 'out', self.product_search_var, self.placeholder_val))
         self.product_search_bar.bind('<KeyRelease>', partial(self.refresh_product_tree_data, True))
         
         self.tree = {
@@ -94,8 +94,8 @@ class Inventory:
         self.product_btn_frame = ttk.Frame(self.main_frame, width=self.frame_width)
         self.product_tree_btn = {
             'refresh': ttk.Button(self.product_btn_frame, text="Refresh", command=partial(self.refresh_product_tree_data, False)),
-            'manage_product_with_entries': ttk.Button(self.product_btn_frame, text="MANAGE PRODUCT w/ ENTRIES", command=partial(self.show_manage_product, 'manage')),
-            'manage_sold': ttk.Button(self.product_btn_frame, text="MANAGE SOLD", command=partial(self.show_manage_product, 'manage')),
+            'manage_product_with_entries': ttk.Button(self.product_btn_frame, text="MANAGE PRODUCT & ARRIVED LOGS", command=partial(self.show_manage_product, 'arrived')),
+            'manage_sold': ttk.Button(self.product_btn_frame, text="MANAGE SOLD", command=partial(self.show_manage_product, 'sold')),
             'delete': ttk.Button(self.product_btn_frame, text="DELETE", command=partial(self.show_manage_product, 'delete'))
         }
         self.tree['product'].bind('<<TreeviewSelect>>', self.refresh_detail_trees_data)
@@ -132,6 +132,8 @@ class Inventory:
 
         self.refresh_product_tree_data(False)
         self.root.lift()
+        self.show_manage_product('arrived')
+        self.show_manage_product('sold')
     
     def close_window(self):
         tools.change_window_status(self.window_status, 'is_closed', True)
@@ -164,7 +166,7 @@ class Inventory:
             added_condition = re.escape(added_condition)
             added_condition_sql  += 'WHERE (LOWER(p.name) LIKE LOWER(\'%' + added_condition + '%\') or LOWER(p.description) LIKE LOWER(\'%' + added_condition + '%\'))'
         else:
-            self.product_search_var.set('. . . Search by Name or Description')
+            self.product_search_var.set(self.placeholder_val)
         
         sql = "SELECT p.*, apl_0.entry_date AS last_entry_date FROM public.products as p LEFT OUTER JOIN public.arrived_products_log AS apl_0 ON (apl_0.pkey = (SELECT DISTINCT ON (entry_date) pkey FROM public.arrived_products_log WHERE refproductkey = p.pkey AND entry_date IS NOT NULL ORDER BY entry_date DESC LIMIT 1)) " + added_condition_sql + " GROUP BY p.pkey, apl_0.entry_date ORDER BY p.sku, p.name ASC;"
         rl = conn.run(sql, added_condition=added_condition)
@@ -177,7 +179,7 @@ class Inventory:
             used_stock = int(conn.run("SELECT COALESCE(SUM(qty), 0) FROM public.sold_products_log WHERE refproductkey = :product_key;", product_key=rl[i][0])[0][0])
             product = {
                 'key':str(rl[i][0]),
-                'latest_release_date': rl[i][4].strftime("%Y - %b - %d") if rl[i][4] is not None else "N/A",
+                'latest_release_date': rl[i][4].strftime("%Y - %b - %d") if rl[i][4] is not None else "N / A",
                 'name': str(rl[i][1]),
                 'description': str(rl[i][2]),
                 'sku': str(rl[i][3]),
@@ -195,11 +197,11 @@ class Inventory:
         tree_key = ['arrived', 'sold']
         label_text = ["Arrived \"", "Sold \""]
 
-        sql = ["SELECT ", " FROM ", " WHERE refproductkey = :product_key ", " ORDER BY ", " DESC"]
+        sql = ["SELECT ", " FROM ", " WHERE refproductkey = :product_key ", " ORDER BY "]
         column = ["*", "spl.pkey, spl.sold_date, spl.qty, spl.sellprice, spl.temp_invoice_id, st.name as sales_type"]
         table = ["public.arrived_products_log", "public.sold_products_log as spl,public.sales_types as st"]
         additional_condition = ["", "AND spl.refsalestypekey = st.pkey"]
-        order_by = ["eta, entry_date", "sold_date"]
+        order_by = ["entry_date DESC, eta DESC", "sold_date DESC"]
 
         args = [[2,3,4,5], [1,4,2,3,5]]
         conn = Connection(user="postgres", password=self.db_password, database="pikacenter")
@@ -207,13 +209,13 @@ class Inventory:
         for i in range(len(tree_key)):
             self.tree[tree_key[i]].delete(*self.tree[tree_key[i]].get_children())
             self.subtitle_label[tree_key[i]].configure(text= label_text[i] + self.tree['product'].item(selected_product_key)['values'][3] + "\" Product Logs")
-            rl_detail = conn.run(sql[0] + column[i] + sql[1] + table[i] + sql[2] + additional_condition[i] + sql[3] + order_by[i] + sql[4], product_key=selected_product_key)
+            rl_detail = conn.run(sql[0] + column[i] + sql[1] + table[i] + sql[2] + additional_condition[i] + sql[3] + order_by[i], product_key=selected_product_key)
             
             total_buy_price = 0
             for j in range(len(rl_detail)):
                 if tree_key[i] == 'arrived':
                     total_buy_price += int(rl_detail[j][5])
-                val_args = ["N/A" if rl_detail[j][args[i][k]] is None else tools.create_pretty_numerical(rl_detail[j][args[i][k]]) if isinstance(rl_detail[j][args[i][k]], Decimal) or isinstance(rl_detail[j][args[i][k]], int) else rl_detail[j][args[i][k]].strftime("%Y - %b - %d") if isinstance(rl_detail[j][args[i][k]], date) else rl_detail[j][args[i][k]] for k in range(len(args[i]))]
+                val_args = ["N / A" if rl_detail[j][args[i][k]] is None else tools.create_pretty_numerical(rl_detail[j][args[i][k]]) if isinstance(rl_detail[j][args[i][k]], Decimal) or isinstance(rl_detail[j][args[i][k]], int) else rl_detail[j][args[i][k]].strftime("%Y - %b - %d") if isinstance(rl_detail[j][args[i][k]], date) else rl_detail[j][args[i][k]] for k in range(len(args[i]))]
                 self.tree[tree_key[i]].insert('', 'end', iid=rl_detail[j][0], values=(j+1, *val_args))
 
             if tree_key[i] == 'arrived':
@@ -232,21 +234,18 @@ class Inventory:
         action_type = args[0]
         selected_product_keys = ""
 
-        if action_type == 'manage':
+        if action_type == 'arrived' or action_type == 'sold':
             selected_product_keys = self.tree['product'].selection()
-            if len(selected_product_keys) == 0:
-                Alert(self.child_roots, "Produk harus dipilih terlebih dahulu sebelum bisa diatur!")
-                return
-            else:
+            if len(selected_product_keys) == 1:
                 selected_product_keys = selected_product_keys[0]
 
-        ManageProduct(self.child_windows_status, self.child_roots, self.db_password, self, action_type, selected_product_keys)
+        ManageProduct(self.child_windows_status, self.child_roots, self.db_password, action_type, selected_product_keys)
 
     def delete_product(self, *args):
         selected_product_key = self.tree['product'].selection()[0]
 
         if len(selected_product_key) == 0:
-            Alert("Produk harus dipilih terlebih dahulu sebelum bisa dihapus!")
+            Alert(self.child_roots, "Please select a listed product before managing it!")
             return
         
         res_list = [None]
