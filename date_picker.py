d912053f593fbf48a55542f68e5f646bb2bbdfe6
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2021-07-25 01:38:00 +0700

    Update date_picker.py

--- date_picker.py
+++ date_picker.py
@@ -1,10 +1,11 @@
 from tkinter import *
 from tkinter import ttk
 from tkcalendar import Calendar
+from datetime import date
 import tools
 
 class DatePicker:
-    def __init__(self, parent_child_roots_list, string_var_ref):
+    def __init__(self, parent_child_roots_list, selected_str_var, counter_str_var):
         window_size = {'width': "300", 'height': "250"}
 
         self.root = Toplevel()
@@ -12,19 +13,23 @@ class DatePicker:
         # parent_child_roots_list.append(self.child_roots)
         parent_child_roots_list.append(self.root)
         
+        self.root.protocol('WM_DELETE_WINDOW', self.clear_string_var)
         self.root.title("Pika Center Invoicing Program - Date Pickerr")
         self.root.geometry(tools.generate_tk_geometry(window_size))
         self.root.resizable(False, False)
 
         main_frame = ttk.Frame(self.root)
 
-        string_var_ref.set('')
-        calendar = Calendar(main_frame, textvariable=string_var_ref, showweeknumbers=False, showothermonthdays=False, date_pattern='y-mm-dd')
-        calendar.bind("<<CalendarSelected>>", self.destroy_window)
-        calendar.grid(column=0, row=0)
+        self.selected_str_var = selected_str_var
+        self.counter_str_var = counter_str_var
+        self.selected_str_var.set('')
+        self.root.after(1)
+        self.calendar = Calendar(main_frame, textvariable=self.selected_str_var, showweeknumbers=False, showothermonthdays=False, date_pattern='y-mm-dd')
+        self.calendar.bind("<<CalendarSelected>>", self.destroy_window)
+        self.calendar.grid(column=0, row=0)
         
         main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
-        calendar.grid(column=0, row=0)
+        self.calendar.grid(column=0, row=0)
 
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
@@ -33,5 +38,16 @@ class DatePicker:
 
         self.root.lift()
     
+    def clear_string_var(self, *args):
+        self.destroy_window(None)
+        
     def destroy_window(self, event):
+        self.calendar.configure(textvariable=StringVar())
         self.root.destroy()
+
+        curr_string_var_val = self.selected_str_var.get()
+        if len(curr_string_var_val) > 0:
+            self.selected_str_var.set(date.fromisoformat(curr_string_var_val).strftime('%Y - %b - %d'))
+            self.counter_str_var.set('')
+            if event is None:
+                self.selected_str_var.set('')
